97b9de063e6b332afcd3fe7ad8b22e7cf9018e12
hoangthaiduong <hoangthaiduong@gmail.com>
2021-07-08 11:14:09 -0600

    Fixed "idx2_lib.hpp" to "idx2.hpp" in idx2_examples.cpp

--- src/idx2_examples.cpp
+++ src/idx2_examples.cpp
@@ -1,5 +1,4 @@
-#define idx2_Implementation // define this only once in your code before including idx2_lib.hpp
-//#include "idx2_lib.hpp"
+#define idx2_Implementation // define this only once in your code before including idx2.hpp
 #include "idx2.hpp"
 #include "stdio.h"
 
